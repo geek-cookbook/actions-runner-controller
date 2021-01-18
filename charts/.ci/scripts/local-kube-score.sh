341fef6edeaa89ae2b7c73f81d9d7031110d703e
David Young <davidy@funkypenguin.co.nz>
2021-01-18 15:15:40 +1300

    Try again...
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- charts/.ci/scripts/local-kube-score.sh
+++ charts/.ci/scripts/local-kube-score.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-helm template --values charts/.ci/values-kube-score.yaml charts/* -f charts/.ci/values-kube-score.yaml | kube-score score - \
+
+for chart in `ls charts`;
+do
+helm template --values charts/$chart/ci/ci-values.yaml charts/$chart | kube-score score - \
     --ignore-test pod-networkpolicy \
     --ignore-test deployment-has-poddisruptionbudget \
     --ignore-test deployment-has-host-podantiaffinity \
     --enable-optional-test container-security-context-privileged \
     --enable-optional-test container-security-context-readonlyrootfilesystem \
     --ignore-test container-security-context
+done
